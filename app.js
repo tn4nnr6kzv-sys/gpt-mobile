fd2d390bd0b6595f8566a39c3dc2c6fcb4fe31e3
tn4nnr6kzv-sys <tn4nnr6kzv@privaterelay.appleid.com>
2026-07-04 17:29:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
   // incrémenter cette valeur ET le CACHE_NAME de sw.js à l'identique (ex. ici "v1.8.0" ->
   // cache "golftracker-mobile-1.8.0"). Changer le nom du cache est ce qui force la purge et
   // garantit que la nouvelle version s'installe proprement.
-  var APP_BUILD = "v1.9.0";
+  var APP_BUILD = "v1.9.1";
 
   var LS_COURSES = "gtm_courses_v1";
   var LS_ROUNDS = "gtm_rounds_v1";
